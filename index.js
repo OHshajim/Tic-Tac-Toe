325c048ec85af2a24cc454fc1da59f22c023ef4f
OHshajim <ajshajimmax@gmail.com>
2024-02-20 23:59:05 +0600

    add feature

---
+++ index.js
@@ -0,0 +1,31 @@
+const boxes = document.querySelectorAll(".box")
+const Rbox = document.getElementById("R-btn")
+let turnO = true;
+const winPattarns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+for (const box of boxes) {
+    box.addEventListener("click", function () {
+        if (turnO) {
+             box.innerText = "O";
+            turnO = false ;
+            }
+        else{
+            box.innerText = "X";
+            turnO = true ;
+        }
+        box.disabled = true ;
+    })
+    Rbox.addEventListener("click",function(){
+        box.innerText = "";
+        box.disabled = false ;
+    })
+}
