1cd30730804bbaad39d97f4b508d18718dc68ff3
OHshajim <ajshajimmax@gmail.com>
2024-02-21 18:13:33 +0600

    game start

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const boxes = document.querySelectorAll(".box")
 const Rbox = document.getElementById("R-btn")
+const text = document.getElementById("h1")
 let turnO = true;
-const winPattarns = [
+const winPatterns = [
     [0, 1, 2],
     [0, 3, 6],
     [0, 4, 8],
@@ -23,15 +24,40 @@ for (const box of boxes) {
             turnO = true;
         }
         box.disabled = true;
+    // win screen
+        checkWin()
+
     })
-   
 
 
-
+    // reset button 
     Rbox.addEventListener("click", function () {
         box.innerText = "";
         box.disabled = false;
+        text.innerText = "Tic Tac Toe"
     })
 }
 
 
+function checkWin() {
+    for (let patterns of winPatterns) {
+
+        let first = boxes[patterns[0]].innerText
+        let second = boxes[patterns[1]].innerText;
+        let third = boxes[patterns[2]].innerText;
+
+        if (first == "" && second == "" && third == "")return ;
+            if (first === second && second === third) {
+                console.log("win");
+                text.innerText = `Winner is ${first}`
+                disabledBoxes();
+
+            }
+    }
+}
+
+function disabledBoxes(){
+    for (const box of boxes) {
+        box.disabled = true
+    }
+}
